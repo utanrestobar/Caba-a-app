03dfa1a33d3329b14e9b8820dd49db662afc17aa
utanrestobar <utan.compras@gmail.com>
2026-08-06 22:13:45 -0300

    Implement service worker with caching strategies

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE = 'cabana-v3';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        if (
+          response &&
+          response.status === 200 &&
+          event.request.url.startsWith(self.location.origin)
+        ) {
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put(event.request, copy);
+          });
+        }
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+
+});
