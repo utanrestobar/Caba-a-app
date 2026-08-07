e582f05d1a012ca4dd45bd2224634c42113631b4
utanrestobar <utan.compras@gmail.com>
2026-08-06 21:27:52 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'cabana-v2';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
